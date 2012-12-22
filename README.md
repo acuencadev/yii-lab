d22d142da5fcb716a9d9d64de48406df12169053
Amador <sphi02ac@gmail.com>
2012-12-22 10:54:27 -0400

    Agregada la descripción del proyecto al README.md y ajustes varios.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Yii-lab
+
+Lab de un CRUD en Yii con MySQL y un reporte FPDF.
+
+## Librerias utilizadas:
+* [Yii 1.1.12] (http://www.yiiframework.com/)
+* [jQuery 1.8.3] (http://jquery.com/)
+* [FPDF 1.7] (http://www.fpdf.org/)
+
+## Configuración
+* Descargar Yii framework y extraer la carpeta framework en la raíz del proyecto
+
+* Ejecutar el script protected/data/schema.mysql.sql
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -49,7 +49,10 @@
 	<div class="clear"></div>
 
 	<div id="footer">		
-		<a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.es"><img alt="Licencia Creative Commons" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Yii lab</span> por <a xmlns:cc="http://creativecommons.org/ns#" href="https://github.com/acuenca/yii-lab" property="cc:attributionName" rel="cc:attributionURL">Amador Cuenca</a> se encuentra bajo una <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.es">Licencia Creative Commons Atribución-CompartirIgual 3.0 Unported</a>.
+		<a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.es">
+			<img alt="Licencia Creative Commons" style="border-width:0" src="<?php echo Yii::app()->request->baseUrl; ?>/images/88x31.png" />
+		</a><br />
+		<span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Yii lab</span> por <a xmlns:cc="http://creativecommons.org/ns#" href="https://github.com/acuenca/yii-lab" property="cc:attributionName" rel="cc:attributionURL">Amador Cuenca</a> se encuentra bajo una <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.es">Licencia Creative Commons Atribución-CompartirIgual 3.0 Unported</a>.
 	</div><!-- footer -->
 
 </div><!-- page -->
